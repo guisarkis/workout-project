3a041daaef4fba8ac220ec35108e9457a550ef36
guisarkis <10195273+guisarkis@users.noreply.github.com>
2020-04-07 14:43:06 -0300

    Complete. Still not finished

---
+++ resources/src/workoutArray.js
@@ -0,0 +1,3 @@
+export default expWorkoutArray = [[0, 45], [0, 15], [0, 45], [0, 15], [0, 45], [0, 15], [0, 45], [0, 15], [0, 45], [0, 15], [0, 45], [0, 15], [0, 45], [0, 15], [0, 45], [0, 15], [0, 45], [0, 15], [0, 0]];
+
+// This didn't work in combination with my first try
